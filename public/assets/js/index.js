b60df0fb3baf6ab64c92232d215487360d53c097
Amplify <155575594+not-amplify@users.noreply.github.com>
2024-02-26 09:39:11 -0500

    fixed blob

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -13,4 +13,19 @@ function topbar() {
   night.innerHTML += '<span class="names">Night</span>';
 };
 
+const blob = document.getElementById('blob');
+
+document.body.onpointermove = event => {
+  const { clientX, clientY } = event;
+
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, {
+    duration: 3000,
+    fill: 'forwards'
+  });
+}
+
+
 topbar()
